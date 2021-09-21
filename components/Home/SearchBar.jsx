9f9110bd65b165626dbb08fc50392de5cd3c7f80
osadavc <osadavidath@gmail.com>
2021-09-21 13:46:52 +0530

    Created API request to get location items

--- components/Home/SearchBar.jsx
+++ components/Home/SearchBar.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function SearchBar({ setCity }) {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    getLocations();
+  }, [input]);
+
+  const getLocations = () => {
+    const MAP_BOX_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${
+      input ? input : "San Francisco"
+    }.json?worldview=cn&access_token=pk.eyJ1Ijoib3NhZGF0aGVjb2RlciIsImEiOiJja3R0czR2Z3IwM3l2Mm9tcGR4MHU3MzA5In0.n8P7tFoXn1G-wumFNjeL2g`;
+
+    return fetch(MAP_BOX_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        return json.features.map((item) => item.place_name);
+      })
+      .then((locations) => console.log(locations));
+  };
+
+  const selectLocation = (selected) => {
+    setCity(selected);
+  };
+
   return (
     <View
       style={{
@@ -26,8 +49,9 @@ export default function SearchBar({ setCity }) {
           paddingHorizontal: 15,
           flex: 1,
         }}
+        value={input}
         onChangeText={(text) => {
-          setCity(text);
+          setInput(text);
         }}
       />
       <View
